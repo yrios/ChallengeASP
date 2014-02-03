3ea19bfd3677f011f318133d400a88187e1eb3bb
NativApps Inc <yrios@nativapps.com>
2014-02-03 09:57:13 -0500

    second commit

---
+++ Challenge/Challenge/Mapping/EntryMap.cs
@@ -0,0 +1,21 @@
+﻿using Challenge.Models;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Challenge.Mapping
+{
+    public class EntryMap : ClassMap<Entry>
+    {
+        public EntryMap()
+        {
+            Id( x => x.id );
+            Map( x => x.creationDate);
+            Map( x => x.title);
+            Map( x => x.content);
+            References(x => x.user);
+        }
+    }
+}
